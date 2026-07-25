c08237a644ac920c0fc505fb69506228ca2a5725
Nagamani Maddali <YOUR_GITHUB_EMAIL>
2026-07-25 19:00:56 +0530

    Updated app for deployment

--- Backend/app.py
+++ Backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask_cors import CORS 
+import os
 from gemini import generate_question, generate_next_question, generate_report
 
 app = Flask(__name__)
@@ -77,4 +78,5 @@ def report():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
